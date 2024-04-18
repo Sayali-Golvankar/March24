67c6f3719328ca2641eb41a278ffb74db0bf2652
Sayali <sayaligolvankarpk@gmail.com>
2024-04-18 22:34:38 +0530

    second file create

---
+++ Room - Copy.java
@@ -0,0 +1,17 @@
+public class Room {
+  
+   float height;
+   float breath;
+   float width;
+   float volume;
+public static void main (String atg[]){
+Room r= new Room();
+float a=r.volume ();
+   System.out.println(a);
+  } 
+float volume(){
+   volume = height*width*breath;
+return volume;
+      
+}
+}
